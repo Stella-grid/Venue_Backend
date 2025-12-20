e694bd88d17eae16c8f6d1b4f85363ed67976845
Stella-grid <tchakounteingrid27@gmail.com>
2025-12-20 05:12:15 +0100

    cors update

--- config/settings.py
+++ config/settings.py
@@ -145,12 +145,24 @@ CORS_ALLOWED_ORIGINS = [
 ] 
 
 CORS_ALLOW_HEADERS = [
-    "accept",
-    "authorization",
-    "content-type",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
         # optional for local testing
 
